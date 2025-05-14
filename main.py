bcb68fbc5b81fc676ac3952f9aa055ab58dc0ef1
Łukasz Kruk <153982527+Dzimmey@users.noreply.github.com>
2025-05-14 22:57:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 from fastapi import FastAPI, HTTPException, Depends
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import APIKeyHeader
 from executor import open_positions
 from selector import get_top_symbols
+from dotenv import load_dotenv
 import os
 
-app = FastAPI()
+# --- Load .env variables ---
+load_dotenv()
 
-# --- CORS middleware (frontend <-> backend) ---
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # możesz to zmienić na konkretny adres np. ["http://localhost:3000"]
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# --- API Init ---
+app = FastAPI()
 
 # --- API Key Security ---
 API_KEY = os.getenv("API_KEY")
-api_key_header = APIKeyHeader(name="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")
+api_key_header = APIKeyHeader(name="X-API-Key")
 
 def verify_api_key(key: str = Depends(api_key_header)):
     if key != API_KEY:
